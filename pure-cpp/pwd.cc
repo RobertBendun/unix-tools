4b853e539f317aa97e4bc18ff4705a2783456e6a
Robert Bendun <rob.bendun@gmail.com>
2023-02-09 03:30:56 +0100

    pwd: added option parsing

--- pure-cpp/pwd.cc
+++ pure-cpp/pwd.cc
@@ -1,7 +1,34 @@
 #include <filesystem>
 #include <iostream>
+#include <span>
+#include <algorithm>
 
-int main()
+
+int main(int argc, char **argv)
 {
-	std::cout << std::filesystem::current_path().c_str() << std::endl;
+	std::span<char*> args(argv+1, argv+argc);
+
+	bool l_spotted = false;
+	bool p_spotted = false;
+
+	for (std::string_view arg : args) {
+		if (arg == "-P") { l_spotted = false; p_spotted = true; continue; }
+		if (arg == "-L") { p_spotted = false; l_spotted = true; continue; }
+
+		std::cerr << "pwd: unrecognized option: " << arg << std::endl;
+		return 1;
+	}
+
+	if (!l_spotted and !p_spotted) {
+		p_spotted = true;
+	}
+
+	auto const current_path = std::filesystem::current_path();
+
+	if (l_spotted) {
+		throw std::runtime_error("unimplemented");
+	}
+
+	// It seems that C++ handles -P option by default?
+	std::cout << current_path.string() << std::endl;
 }
